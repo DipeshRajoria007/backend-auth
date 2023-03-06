c0b1b678630f43b22492b32e903e5eb771690e2a
Dipesh Rajoria <dipeshrajoria@MacBook-Air.local>
2023-03-07 01:33:42 +0530

    REST API for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,8 +1,9 @@
 const asyncHandler = require("express-async-handler");
-
+const Goal = require("../models/goal.model");
 const getGoals = asyncHandler(async (req, res) => {
+  const goals = await Goal.find();
   res.json({
-    message: "Get goals",
+    message: goals,
   });
 });
 const addGoals = asyncHandler(async (req, res) => {
@@ -11,18 +12,41 @@ const addGoals = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("please provide a text");
   }
+  const goal = await Goal.create({
+    text,
+  });
   res.json({
-    message: "Added goal",
+    message: goal,
   });
 });
 const updateGoals = asyncHandler(async (req, res) => {
   const id = req.params.id;
+  const { text } = req.body;
+  const goal = await Goal.findById(id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+  console.log(req.body);
+  const updatedGoal = await Goal.findByIdAndUpdate(
+    id,
+    { text },
+    {
+      new: true,
+    }
+  );
   res.json({
-    message: `Update goal ${id}`,
+    message: updatedGoal,
   });
 });
 const deleteGoals = asyncHandler(async (req, res) => {
   const id = req.params.id;
+  const goal = await Goal.findById(id);
+  if (!goal) {
+    res.status(404);
+    throw new Error({ message: "Goal not found" });
+  }
+  const deletedGoal = await Goal.findByIdAndDelete(id);
   res.json({
     message: `Delete goal ${id}`,
   });
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const route = require("./routes/goalRoutes");
 const dotenv = require("dotenv").config();
+const colors = require("colors");
 const { errorHandler } = require("./Middleware/errorMiddleware");
+
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
+connectDB();
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: false }));
 app.use("/api/goals", route);
 app.use(errorHandler);
 app.listen(port, () => {
